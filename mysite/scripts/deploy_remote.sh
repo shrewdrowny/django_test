c5fd7ed940bd38f9b41058e9e7c615a4b2147538
shrewdrowny <soyongjun00@gmail.com>
2025-11-07 02:55:32 +0000

    Trouble Shooting git clone

--- mysite/scripts/deploy_remote.sh
+++ mysite/scripts/deploy_remote.sh
@@ -8,56 +8,41 @@ REPO_URL=https://github.com/shrewdrowny/django_test.git
 
 BRANCH=main   # 또는 master
 
-# 1) 디렉토리 준비
+echo "[INFO] Deploying into: ${PROJECT_DIR}"
 
-mkdir -p ${PROJECT_DIR}
+# 0) TEMP DIR 준비
+rm -rf "${TEMP_DIR}"
+mkdir -p "${TEMP_DIR}"
 
-cd ${PROJECT_DIR}
+# 1) repo를 TEMP_DIR 에 clone
+git clone -b ${BRANCH} "${REPO_URL}" "${TEMP_DIR}"
 
-# 2) 레포가 없으면 clone, 있으면 reset to remote
+# 2) repo 안의 mysite/ 내용만 실제 PROJECT_DIR 로 복사
+mkdir -p "${PROJECT_DIR}"
+cp -r ${TEMP_DIR}/mysite/* "${PROJECT_DIR}/"
 
-if [ ! -d .git ]; then
+# TEMP 삭제
+rm -rf "${TEMP_DIR}"
 
- git clone -b ${BRANCH} ${REPO_URL} .
-
-else
-
- git fetch origin
-
- git reset --hard origin/${BRANCH}
-
-fi
-
-# 3) 가상환경 준비
-
+# 3) 가상환경
+cd "${PROJECT_DIR}"
 python3 -m venv venv || true
-
 source venv/bin/activate
 
 pip install --upgrade pip
-
 pip install -r requirements.txt
 
-# 4) 환경변수 파일(.env)이 필요하면 복사/설정 (수동으로 .env를 서버에 배치)
-
-# cp /home/vagrant/.env.production .env || true
-
-# 5) 장고 작업
-
-python3 manage.py(http://manage.py) migrate --noinput
-
-python3 manage.py(http://manage.py) makemigrations --noinput
-
+# 4) Django 작업
+python3 manage.py makemigrations --noinput || true
 python3 manage.py migrate --noinput
-
 python3 manage.py collectstatic --noinput
 
-# 6) 기존 runserver 종료 (비정상적인 경우도 있으므로 pkill로 처리)
-
+# 5) 기존 runserver 종료
 pkill -f "manage.py runserver" || true
 
-# 7) runserver 백그라운드 실행, 로그는 /var/log/pybo_run.log
-
-nohup python3 manage.py runserver 0.0.0.0:8000 > /var/log/pybo_run.log 2>&1 &
+# 6) runserver 재시작
+nohup python3 manage.py runserver 0.0.0.0:8000 \
+    > ${PROJECT_DIR}/mysite_run.log 2>&1 &
 
 echo "DEPLOY_OK $(date)"
+
